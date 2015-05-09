a88a328b375b58550af37dc2d433358c5986ac19
Around the Lines <wangqile123@gmail.com>
2015-05-09 19:56:25 -0400

    Added Chart.js and arduino file

---
+++ smartPillow.ino/smartPillow.ino/smartPillow.ino.ino
@@ -0,0 +1,236 @@
+/*****************************************************************************/
+//	Function:    Get the accelemeter of X/Y/Z axis and print out on the
+//					serial monitor.
+//  Hardware:    3-Axis Digital Accelerometer(��16g)
+//	Arduino IDE: Arduino-1.0
+//	Author:	 Frankie.Chu
+//	Date: 	 Jan 11,2013
+//	Version: v1.0
+//	by www.seeedstudio.com
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+/*******************************************************************************/
+
+#include <Wire.h>
+#include <ADXL345.h>
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "Qile";
+char pass[] = "asdfghjkl";
+int keyIndex = 0;
+int sensorPin = A5;
+double sensorValue = 0.0;
+
+int status = WL_IDLE_STATUS;
+char server[] = "api.pushingbox.com";
+//IPAddress ip();
+String data;
+
+WiFiClient client;
+
+
+unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
+
+ADXL345 adxl; //variable adxl is an instance of the ADXL345 library
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("No WiFi shield");
+    while (true);
+  }
+
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+
+    delay(10000);
+  }
+
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+
+  adxl.powerOn();
+
+  //set activity/ inactivity thresholds (0-255)
+  adxl.setActivityThreshold(75); //62.5mg per increment
+  adxl.setInactivityThreshold(75); //62.5mg per increment
+  adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
+
+  //look of activity movement on this axes - 1 == on; 0 == off
+  adxl.setActivityX(1);
+  adxl.setActivityY(1);
+  adxl.setActivityZ(1);
+
+  //look of inactivity movement on this axes - 1 == on; 0 == off
+  adxl.setInactivityX(1);
+  adxl.setInactivityY(1);
+  adxl.setInactivityZ(1);
+
+  //look of tap movement on this axes - 1 == on; 0 == off
+  adxl.setTapDetectionOnX(0);
+  adxl.setTapDetectionOnY(0);
+  adxl.setTapDetectionOnZ(1);
+
+  //set values for what is a tap, and what is a double tap (0-255)
+  adxl.setTapThreshold(50); //62.5mg per increment
+  adxl.setTapDuration(15); //625us per increment
+  adxl.setDoubleTapLatency(80); //1.25ms per increment
+  adxl.setDoubleTapWindow(200); //1.25ms per increment
+
+  //set values for what is considered freefall (0-255)
+  adxl.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
+
+  //setting all interrupts to take place on int pin 1
+  //I had issues with int pin 2, was unable to reset it
+  adxl.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
+
+  //register interrupt actions - 1 == on; 0 == off
+  adxl.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
+  adxl.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
+  adxl.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+
+}
+
+void loop() {
+
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  //Boring accelerometer stuff
+  int x, y, z;
+  adxl.readXYZ(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+  // Output x,y,z values
+  /*Serial.print("values of X , Y , Z: ");
+  Serial.print(x);
+  Serial.print(" , ");
+  Serial.print(y);
+  Serial.print(" , ");
+  Serial.println(z);*/
+
+  double xyz[3];
+  double ax, ay, az;
+  adxl.getAcceleration(xyz);
+  ax = xyz[0];
+  ay = xyz[1];
+  az = xyz[2];
+  Serial.print("X =");
+  Serial.print(ax);
+  Serial.println(" g");
+  Serial.print("Y =");
+  Serial.print(ay);
+  Serial.println(" g");
+  Serial.print("Z =");
+  Serial.print(az);
+  Serial.println(" g");
+  Serial.println("**********************");
+
+
+  logData(ax, ay, az);
+}
+
+void logData(double x, double y, double z)
+{
+  sensorValue = analogRead(sensorPin);
+  
+  char tostr[150];
+  sprintf(tostr, "GET /pushingbox?devid=vF68638C4307149D&RawAcc=%f,%f&RawMic=%f HTTP/1.1", x, z, sensorValue);
+
+  data = tostr;
+
+  client.stop();
+
+  if (client.connect(server, 80)) {
+    Serial.println("connecting...");
+    Serial.println(data);
+    //HTTP request
+    client.println(data);
+    client.println("Host: api.pushingbox.com");
+    client.println("Connection:close");
+    client.println();
+    delay(2000);
+  } else {
+    Serial.println("connection failed");
+  }
+}
+
+void printWifiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI): ");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+/*
+
+//Fun Stuff!
+//read interrupts source and look for triggerd actions
+
+//getInterruptSource clears all triggered actions after returning value
+//so do not call again until you need to recheck for triggered actions
+ byte interrupts = adxl.getInterruptSource();
+
+// freefall
+if(adxl.triggered(interrupts, ADXL345_FREE_FALL)){
+  Serial.println("freefall");
+  //add code here to do when freefall is sensed
+}
+
+//inactivity
+if(adxl.triggered(interrupts, ADXL345_INACTIVITY)){
+  Serial.println("inactivity");
+   //add code here to do when inactivity is sensed
+}
+
+//activity
+if(adxl.triggered(interrupts, ADXL345_ACTIVITY)){
+  Serial.println("activity");
+   //add code here to do when activity is sensed
+}
+
+//double tap
+if(adxl.triggered(interrupts, ADXL345_DOUBLE_TAP)){
+  Serial.println("double tap");
+   //add code here to do when a 2X tap is sensed
+}
+
+//tap
+if(adxl.triggered(interrupts, ADXL345_SINGLE_TAP)){
+  Serial.println("tap");
+   //add code here to do when a tap is sensed
+} */
