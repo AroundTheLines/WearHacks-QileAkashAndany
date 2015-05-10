f16dd0aed364e347904f40fa1cd6cfcafa01d574
Around the Lines <wangqile123@gmail.com>
2015-05-10 01:34:19 -0400

    experimental changes. DO NOT SYNC.

--- js/pillowCase.js
+++ js/pillowCase.js
@@ -1,3 +1,25 @@
+angular.module('myModule', ['chart.js']);
+var currentIndex = 1;
+var link = "https://script.google.com/macros/s/AKfycby0Z_QxgA2KyqQr8QStRj66M6V6XLiLlYhMgfs3eiJCxzvjZZ3Y/exec?rowNo=";
+
+var currentLink = link.concat(currentIndex);
+
+var time = [];
+var xMovement = [];
+var soundRecorder = [];
+
+setInterval(function(){//runs every 1 second
+    $.get(currentLink,function(data){//gets data from the link^
+        if(data[0].toString()==null){//break out of the loop and reinstantiate it, or a continue
+            //feature would be nice.
+        }
+        time.push(data[0].toString());
+        xMovement.push(data[1].toString());
+        soundRecorder.push(data[2].toString());
+    });
+    alert(time);
+},1000);
+
 var data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
